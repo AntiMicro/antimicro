782a72c587b57b961af0ed10f4c7b0878946da11
Jeff Backus <jeff@jsbackus.com>
2016-12-17 22:12:55 -0500

    First pass at importing Git SHA1 and Refspec. Compiles, but doesn't link...

--- src/common.h
+++ src/common.h
@@ -128,11 +128,21 @@ namespace PadderCommon
     const int ANTIMICRO_MINOR_VERSION = PROJECT_MINOR_VERSION;
     const int ANTIMICRO_PATCH_VERSION = PROJECT_PATCH_VERSION;
 
-    const QString programVersion = (ANTIMICRO_PATCH_VERSION > 0) ?
+    // Defined in GitInfo.cpp / GitInfo.cpp.in. Data provided by CMake.
+    extern const char ANTIMICRO_GIT_SHA1[];
+    extern const char ANTIMICRO_GIT_REFSPEC[];
+
+    const QString programVersionStr = (ANTIMICRO_PATCH_VERSION > 0) ?
         QString("%1.%2.%3").arg(ANTIMICRO_MAJOR_VERSION)
             .arg(ANTIMICRO_MINOR_VERSION).arg(ANTIMICRO_PATCH_VERSION) :
         QString("%1.%2").arg(ANTIMICRO_MAJOR_VERSION)
             .arg(ANTIMICRO_MINOR_VERSION);
+
+    const QString programVersion =
+      ( ANTIMICRO_GIT_SHA1[0] != NULL ) ?
+      QString("%1 (%2 %3)").arg(programVersionStr, ANTIMICRO_GIT_REFSPEC,
+				ANTIMICRO_GIT_SHA1) :
+      programVersionStr;
 
     extern QWaitCondition waitThisOut;
     extern QMutex sdlWaitMutex;
